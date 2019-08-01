16dd238c3b1f08a6867ab03a4d70e089c5a7e950
vtesoho <vtesoho@163.com>
2019-08-01 23:37:31 +0800

    基础类型 ;

--- greeter.ts
+++ greeter.ts
@@ -1,26 +1,45 @@
-class User {
-  fullName:string
-  firstName:string
-  lastName:string
-  constructor(firstName:string, lastName:string){
-    this.firstName = firstName
-    this.lastName = lastName
-    this.fullName = firstName + ' ' + lastName
-  }
-}
+//布尔
+//只能是true或是false
+let isBool: boolean = false;
+isBool = true;
+// isBool = 1;
+
+
+//数字
+var decLiteral: number = 20  //10进制
+let hexLiteral: number = 0x14  //16进制
+let bingryLiteral: number = 0b10100  //2进制
+let octalLiteral: number = 0o24  //8进制
+
+
+//字符串
+let nameA: string = 'hello'
+nameA = 'word'
+let age: number = 30
+let sentence = `hello ${age}.
+多行也可以
+
+year ${nameA} happy`
+
+
+//数组
+//定义了number类型是不能用其它类型的，下面二种定义方式是一样的
+let list: number[] = [1,3,4,5,6]
+
+let listA: Array<number> = [1,4,5]
+
+// listA.push('2')
+
+
+//元组
+//一种已知类型的数组
+let x: [string, number]
+x = ['hello',20]
+// x = [20,20]  //类型不对
 
 
 
+// console.log(x[0].substr)
+// console.log(x[1].substr)  //index 1  是数字，没有substr
 
-interface Person {
-  firstName: string
-  lastName: string
-}
 
-function greeter(person: Person) {
-  return 'hello ' + person.firstName + ' ' +person.lastName
-}
-
-let user:Person = new User('vte', 'so');
-
-console.log(greeter(user))
