676b87430c7445bea58a601e99070a697ee3bcf9
vtesoho <vtesoho@163.com>
2019-08-07 22:48:15 +0800

    '解构，展开'

--- greeter.js
+++ greeter.js
@@ -1,70 +1,26 @@
-// //块作用域 
-// try {
-//   throw 'Oh no'
-// } catch (e) {
-//   console.log('catch it')
-// }
-// console.log(e)
-var _loop_1 = function (i) {
-    setTimeout(function () {
-        console.log(i);
-    }, 100 * i);
+//展开
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
+        for (var s, i = 1, n = arguments.length; i < n; i++) {
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
+        }
+        return t;
+    };
+    return __assign.apply(this, arguments);
 };
-// function f(input: boolean){
-//   let a = 100
-//   if(input) {
-//     let b = a + 1
-//     return b
-//   }
-//   return b
-// }
-// a++  
-// let a =1
-// a++
-// // 有es2015之前，这样写是没问题的，在es2015之后就不行了
-// function foo(){
-//   return a
-// }
-// foo()
-// let a
-// // let 只能定义一次
-// function f(x) {
-//   var x
-//   var x
-//   if(true){
-//     var x
-//   }
-// }
-// let x=10
-// function st(b){
-//   // let x = 20
-//   console.log(x,b)
-// }
-// st(30)
-// function f(condition,x){
-//   if(condition){
-//     let x = 100
-//     return x
-//   }
-//   return x
-// }
-// f(false,0)
-// f(true,0)
-// function sumMatrix(matrix: number[][]){
-//   let sum = 0
-//   for(let i = 0; i < matrix.length; i++){
-//     let currentRow = matrix[i]
-//     for(let j = 0; j < currentRow.length; j++){
-//       sum += currentRow[j]
-//     }
-//   }
-//   return sum
-// }
-// let matrix = [
-//   [1,2,3],
-//   [4,5,6]
-// ]
-// console.log(sumMatrix(matrix))
-for (var i = 0; i < 10; i++) {
-    _loop_1(i);
-}
+// let first = [1,2]
+// let second = [3,4]
+// //这里是一个浅拷贝，对于bothPlue操作是不对影响 first和 second
+// let bothPlue= [0,...first,...second,5]
+// console.log(bothPlue)
+//对象的展开
+var defauts = {
+    food: 'spicy',
+    price: '$10',
+    ambiance: 'noisy'
+};
+//相同的key会覆盖，后面的会覆盖前面的
+var search = __assign({}, defauts, { food: 'rich' });
+console.log(search);
