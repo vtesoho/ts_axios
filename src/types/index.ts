1c89ad6b1b32e8ce7f5ec67e5e32baef27b40bdf
vtesoho <vtesoho@163.com>
2019-09-18 22:58:17 +0800

    '拦截器'

--- src/types/index.ts
+++ src/types/index.ts
@@ -66,4 +66,17 @@ export interface AxiosInstance extends Axios{
 }
 
 
+export interface AxiosInterceptorManager<T>{
+  use(resolved:ResolvedFn<T>,rejected: RejectedFn): number
+
+  eject(id:number):void
+}
+
+export interface ResolvedFn<T> {
+  (val:T):T|Promise<T>
+}
+
+export interface RejectedFn {
+  (error:any) :any
+}
 
